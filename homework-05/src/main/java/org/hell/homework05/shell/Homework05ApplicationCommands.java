984cd581e49b805fbe3fdee8e6865466bef3a1e3
RN3ARA <rn3ara@gmail.com>
2021-03-25 12:24:43 +0300

    Added shell methods for genres and authors

--- homework-05/src/main/java/org/hell/homework05/shell/Homework05ApplicationCommands.java
+++ homework-05/src/main/java/org/hell/homework05/shell/Homework05ApplicationCommands.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.hell.homework05.domain.Author;
 import org.hell.homework05.domain.Book;
 import org.hell.homework05.domain.Genre;
+import org.hell.homework05.service.AuthorService;
 import org.hell.homework05.service.BookService;
+import org.hell.homework05.service.GenreService;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
@@ -14,6 +16,8 @@ import org.springframework.shell.standard.ShellOption;
 public class Homework05ApplicationCommands {
 
     private final BookService bookService;
+    private final AuthorService authorService;
+    private final GenreService genreService;
 
     @ShellMethod(key = {"insert-book"}, value = "Insert book into table")
     public String insertBook(@ShellOption(defaultValue = "First Name") String authorFirstName,
@@ -57,4 +61,81 @@ public class Homework05ApplicationCommands {
     public String countBooks() {
         return String.valueOf(bookService.count());
     }
+
+    @ShellMethod(key = {"insert-author"}, value = "Insert author into table")
+    public String insertAuthor(@ShellOption(defaultValue = "First Name") String firstName,
+                             @ShellOption(defaultValue = "Last Name") String lastName) {
+        long id = authorService.insert(new Author(firstName, lastName));
+        return String.format("Inserted author with id %d", id);
+    }
+
+    @ShellMethod(key = {"delete-author"}, value = "Delete author from table")
+    public void deleteAuthor(@ShellOption(defaultValue = "0") String id) {
+        authorService.deleteById(Long.parseLong(id));
+    }
+
+    @ShellMethod(key = {"get-author"}, value = "Get author from table by its Id")
+    public String getAuthor(@ShellOption(defaultValue = "0") String id) {
+        return authorService.getById(Long.parseLong(id)).toString();
+    }
+
+    @ShellMethod(key = {"get-all-authors"}, value = "Get all authors from table")
+    public String getAllAuthors() {
+        return authorService.getAll().toString();
+    }
+
+    @ShellMethod(key = {"update-author"}, value = "Update author in table")
+    public String updateAuthor(@ShellOption(defaultValue = "0") String id,
+                             @ShellOption(defaultValue = "First Name") String firstName,
+                             @ShellOption(defaultValue = "Last Name") String lastName) {
+        Author foundAuthor = authorService.getById(Long.parseLong(id));
+        if (foundAuthor != null) {
+            authorService.update(new Author(foundAuthor.getId(), firstName, lastName));
+            return String.format("Updated author with id %s", id);
+        }
+        return "Nothing updated.";
+    }
+
+    @ShellMethod(key = {"count-authors"}, value = "Get count of authors in table")
+    public String countAuthors() {
+        return String.valueOf(authorService.count());
+    }
+
+    @ShellMethod(key = {"insert-genre"}, value = "Insert genre into table")
+    public String insertGenre(@ShellOption(defaultValue = "Name") String name) {
+        long id = genreService.insert(new Genre(name));
+        return String.format("Inserted genre with id %d", id);
+    }
+
+    @ShellMethod(key = {"delete-genre"}, value = "Delete genre from table")
+    public void deleteGenre(@ShellOption(defaultValue = "0") String id) {
+        genreService.deleteById(Long.parseLong(id));
+    }
+
+    @ShellMethod(key = {"get-genre"}, value = "Get genre from table by its Id")
+    public String getGenre(@ShellOption(defaultValue = "0") String id) {
+        return genreService.getById(Long.parseLong(id)).toString();
+    }
+
+    @ShellMethod(key = {"get-all-genres"}, value = "Get all genres from table")
+    public String getAllGenres() {
+        return genreService.getAll().toString();
+    }
+
+    @ShellMethod(key = {"update-genre"}, value = "Update genre in table")
+    public String updateGenre(@ShellOption(defaultValue = "0") String id,
+                             @ShellOption(defaultValue = "Name") String name) {
+        Genre foundGenre = genreService.getById(Long.parseLong(id));
+        if (foundGenre != null) {
+            genreService.update(new Genre(foundGenre.getId(), name));
+            return String.format("Updated genre with id %s", id);
+        }
+        return "Nothing updated.";
+    }
+
+    @ShellMethod(key = {"count-genres"}, value = "Get count of genres in table")
+    public String countGenres() {
+        return String.valueOf(genreService.count());
+    }
+
 }
