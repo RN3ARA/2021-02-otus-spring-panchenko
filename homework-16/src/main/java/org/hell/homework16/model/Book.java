31d51bf3f4fb922005ee8ad842801c14adc37124
RN3ARA <rn3ara@gmail.com>
2021-06-29 00:38:27 +0300

    Replaced initial application for this task

---
+++ homework-16/src/main/java/org/hell/homework16/model/Book.java
@@ -0,0 +1,52 @@
+package org.hell.homework16.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@NamedEntityGraph(name = "book-entity-graph",
+        attributeNodes = {
+        @NamedAttributeNode("author"),
+                @NamedAttributeNode("genre"),
+                @NamedAttributeNode("comments")
+        })
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", referencedColumnName = "id")
+    private Author author;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "genre_id", referencedColumnName = "id")
+    private Genre genre;
+
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Comment> comments;
+
+    public Book(Author author, String title, Genre genre) {
+        this.author = author;
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public Book(long id, Author author, String title, Genre genre) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.genre = genre;
+    }
+}
---
+++ homework-16/src/main/java/org/hell/homework16/service/GenreServiceImpl.java
@@ -0,0 +1,56 @@
+package org.hell.homework16.service;
+
+import org.hell.homework16.model.Genre;
+import org.hell.homework16.repository.GenreRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class GenreServiceImpl implements GenreService {
+
+    private final GenreRepository repository;
+
+    public GenreServiceImpl(GenreRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Genre findById(long id) {
+        return repository.findById(id)
+                .orElse(null);
+    }
+
+    @Override
+    public Genre findByName(String name) {
+        return repository.findByName(name);
+    }
+
+    @Override
+    public Iterable<Genre> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(long id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    @Transactional
+    public Genre save(Genre genre) {
+        return repository.save(genre);
+    }
+
+    @Override
+    @Transactional
+   public void update(Genre genre) {
+        repository.save(genre);
+    }
+
+    @Override
+    public long count() {
+        return repository.count();
+    }
+
+}
